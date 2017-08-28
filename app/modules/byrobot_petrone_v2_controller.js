df6d3cc54935fa42c3d4833fdc2cb51d34a91396
sejong lee <sejong.lee805@gmail.com>
2017-08-28 10:33:07 +0900

    한글 입력시 출력 방식 변경
    (전체 출력 안함 -> 한글만 제외하고 나머지는 출력)

--- app/modules/byrobot_petrone_v2_controller.js
+++ app/modules/byrobot_petrone_v2_controller.js
@@ -851,7 +851,8 @@ Module.prototype.handlerForEntry = function(handler)
                 var charCode = str.charCodeAt(i);
                 if (charCode > 0xFF)  // char > 1 byte since charCodeAt returns the UTF-16 value
                 {
-                    throw new Error('Character ' + String.fromCharCode(charCode) + ' can\'t be represented by a US-ASCII byte.');
+                    // throw new Error('Character ' + String.fromCharCode(charCode) + ' can\'t be represented by a US-ASCII byte.');
+                    continue;
                 }
                 bytes.push(charCode);
             }
