ab4d2b3f50c086e87e3aa3af1aaaff50faa57469
extracold1209 <extracold1209@gmail.com>
2019-03-05 18:35:42 +0900

    scanner/scanner.js 클래스화

--- app/custom_modules/router/scanner/scanner.js
+++ app/custom_modules/router/scanner/scanner.js
@@ -1,31 +1,28 @@
 'use strict';
-const path = require('path');
+const logger = require('../../logger').get();
 
-function Scanner() {
-}
-
-Scanner.prototype.startScan = function(router, extension, config) {
-	var logger = require('../../logger').get();
-	logger.i('scanning...');
-
-	var type = config.hardware.type;
-
-	var scanner = require('./serial');
-	this.scanner = scanner;
-	scanner.startScan(extension, config, function(error, connector) {
-		if(error) {
-			logger.e(error);
-		} else if(connector) {
-			router.connect(connector, config);
+class Scanner {
+	startScan(router, extension, config) {
+		if (!this.scanner) {
+			this.scanner = require('./serial');
 		}
-	}, router);
-};
 
-Scanner.prototype.stopScan = function() {
-	if(this.scanner) {
-		this.scanner.stopScan();
-		this.scanner = undefined;
-	}
-};
+		logger.i('scanning...');
+		this.scanner.startScan(extension, config, function(error, connector) {
+			if(error) {
+				logger.e(error);
+			} else if(connector) {
+				router.connect(connector, config);
+			}
+		}, router);
+	};
+
+	stopScan() {
+		if(this.scanner) {
+			this.scanner.stopScan();
+			this.scanner = undefined;
+		}
+	};
+}
 
 module.exports = new Scanner();
