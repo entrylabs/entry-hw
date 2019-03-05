36b60b37fdf6236e6a8f7c5a9bcbeacf6a60a0d9
extracold1209 <extracold1209@gmail.com>
2019-03-05 12:27:10 +0900

    scanner/serial.js 클래스화

--- app/custom_modules/router/scanner/serial.js
+++ app/custom_modules/router/scanner/serial.js
@@ -1,283 +1,285 @@
 'use strict';
-function Scanner() {
-}
+class Scanner {
+	static get SCAN_INTERVAL_MILLS() {
+		return 1500;
+	}
 
-function checkObject(target) {
-	return Object.prototype.toString.call(target) === '[object Object]';
-}
+	static _isObject(target) {
+		return Object.prototype.toString.call(target) === '[object Object]';
+	}
 
-Scanner.prototype.startScan = function (extension, config, callback, router) {
-	var serialport = require('@serialport/stream'); // modify
-	serialport.Binding = require('@entrylabs/bindings');
-	var self = this;
+	constructor() {
+		this.serialport = require('@serialport/stream');
+		this.serialport.Binding = require('@entrylabs/bindings');
+	}
 
-	self.router = router;
-	self.config = config;
-	self.slaveTimers = {};
-	self.connectors = {};
-	self.scanCount = 0;
-	self.closeConnectors();
-	self.clearTimers();
-	self.scan(serialport, extension, config, callback);
-	self.timer = setInterval(function () {
-		self.scan(serialport, extension, config, callback);
-	}, 1500);
-};
+	startScan(extension, config, callback, router) {
+		this.router = router;
+		this.config = config;
+		this.slaveTimers = {};
+		this.connectors = {};
+		this.scanCount = 0;
+		this.closeConnectors();
+		this.clearTimers();
 
-Scanner.prototype.stopScan = function () {
-	this.config = undefined;
-	this.clearTimers();
-	this.closeConnectors();
-};
+		this.scan(extension, config, callback);
+		this.timer = setInterval(() => {
+			this.scan(extension, config, callback);
+		}, Scanner.SCAN_INTERVAL_MILLS);
+	};
 
-Scanner.prototype.scan = function (serialport, extension, config, callback) {
-	var self = this;
+	stopScan() {
+		this.config = undefined;
+		this.clearTimers();
+		this.closeConnectors();
+	};
 
-	if (self.config != config) return;
-	serialport.list(function (error, devices) {
-		if (error) {
-			if (callback) {
-				callback(error);
+	scan(extension, config, callback) {
+		if (this.config !== config) return;
+		this.serialport.list((error, devices) => {
+			if (error) {
+				if (callback) {
+					callback(error);
+				}
+				return;
 			}
-			return;
-		}
 
-		var serverMode = config.serverMode;
-		var scanType = config.hardware.scanType;
-		var vendor = config.hardware.vendor;
-		var checkComName = config.hardware.comName;
+			var serverMode = config.serverMode;
+			var scanType = config.hardware.scanType;
+			var vendor = config.hardware.vendor;
+			var checkComName = config.hardware.comName;
 
-		if (vendor && checkObject(vendor)) {
-			vendor = vendor[process.platform];
-		}
+			var control = config.hardware.control;
 
-		var control = config.hardware.control;
-		var duration = config.hardware.duration;
-		var firmwarecheck = config.hardware.firmwarecheck;
-		var pnpId = config.hardware.pnpId;
-		var type = config.hardware.type;
-		var selectComPort = config.select_com_port;
+			var duration = config.hardware.duration;
+			var firmwarecheck = config.hardware.firmwarecheck;
+			var pnpId = config.hardware.pnpId;
+			var type = config.hardware.type;
+			var selectComPort = config.select_com_port;
 
-		if (selectComPort && checkObject(selectComPort)) {
-			selectComPort = selectComPort[process.platform];
-		}
+			if (vendor && Scanner._isObject(vendor)) {
+				vendor = vendor[process.platform];
+			}
 
-		var checkComPort = (selectComPort || type === 'bluetooth' || serverMode === 1) || false;
-		var myComPort = config.this_com_port;
+			if (selectComPort && Scanner._isObject(selectComPort)) {
+				selectComPort = selectComPort[process.platform];
+			}
 
+			var checkComPort = (selectComPort || type === 'bluetooth' || serverMode === 1) || false;
+			var myComPort = config.this_com_port;
 
+			if (checkComPort && !myComPort) {
+				this.router.emit('state', 'select_port', devices);
+				callback();
+				return;
+			}
 
-		if (checkComPort && !myComPort) {
-			self.router.emit('state', 'select_port', devices);
-			callback();
-			return;
-		}
-
-		if (scanType == 'data') {
-			if (self.scanCount < 5) {
-				self.scanCount++;
-			} else {
-				if (devices.some(function (device) {
-					var isVendor = false;
-					if (Array.isArray(vendor)) {
-						isVendor = vendor.some(function (v) {
-							return device.manufacturer && device.manufacturer.indexOf(v) >= 0;
-						});
-					} else {
-						if (device.manufacturer && device.manufacturer.indexOf(vendor) >= 0) {
-							isVendor = true;
+			if (scanType === 'data') {
+				if (this.scanCount < 5) {
+					this.scanCount++;
+				} else {
+					if (devices.some(function (device) {
+						var isVendor = false;
+						if (Array.isArray(vendor)) {
+							isVendor = vendor.some(function (v) {
+								return device.manufacturer && device.manufacturer.indexOf(v) >= 0;
+							});
+						} else {
+							if (device.manufacturer && device.manufacturer.indexOf(vendor) >= 0) {
+								isVendor = true;
+							}
 						}
+
+						return device.manufacturer && isVendor;
+					}) === false) {
+						vendor = undefined;
+					}
+				}
+			}
+
+			devices.forEach((device) => {
+				if (this.config !== config) return;
+
+				var isVendor = false;
+				var isComName = false;
+				var comName = device.comName || config.hardware.name;
+
+				if (Array.isArray(vendor)) {
+					isVendor = vendor.some(function (name) {
+						return device.manufacturer && device.manufacturer.indexOf(name) >= 0;
+					});
+				} else if (vendor && device.manufacturer && device.manufacturer.indexOf(vendor) >= 0) {
+					isVendor = true;
+				}
+
+				if (Array.isArray(checkComName)) {
+					isComName = checkComName.some(function (name) {
+						return comName.indexOf(name) >= 0;
+					});
+				} else if (checkComName && comName.indexOf(checkComName) >= 0) {
+					isComName = true;
+				}
+
+				if (!vendor || (device.manufacturer && isVendor) || (device.pnpId && device.pnpId.indexOf(pnpId) >= 0) || isComName || checkComPort) {
+
+					if (checkComPort && comName != myComPort) {
+						return;
 					}
 
-					return device.manufacturer && isVendor;
-				}) == false) {
-					vendor = undefined;
-				}
-			}
-		}
+					// comName = '/dev/tty.EV3-SerialPort';
 
-		devices.forEach(function (device) {
-			if (self.config != config) return;
-
-			var isVendor = false;
-			var isComName = false;
-			var comName = device.comName || config.hardware.name;
-
-			if (Array.isArray(vendor)) {
-				isVendor = vendor.some(function (name) {
-					return device.manufacturer && device.manufacturer.indexOf(name) >= 0;
-				});
-			} else if (vendor && device.manufacturer && device.manufacturer.indexOf(vendor) >= 0) {
-				isVendor = true;
-			}
-
-			if (Array.isArray(checkComName)) {
-				isComName = checkComName.some(function (name) {
-					return comName.indexOf(name) >= 0;
-				});
-			} else if (checkComName && comName.indexOf(checkComName) >= 0) {
-				isComName = true;
-			}
-
-			if (!vendor || (device.manufacturer && isVendor) || (device.pnpId && device.pnpId.indexOf(pnpId) >= 0) || isComName || checkComPort) {
-
-				if (checkComPort && comName != myComPort) {
-					return;
-				}
-
-				// comName = '/dev/tty.EV3-SerialPort';
-
-				var connector = self.connectors[comName];
-				if (connector == undefined) {
-					connector = require('../connector/serial').create();
-					self.connectors[comName] = connector;
-					connector.open(comName, config.hardware, function (error, sp) {
-						if (error) {
-							delete self.connectors[comName];
-							if (callback) {
-								callback(error);
-							}
-						} else {
-							self.setConnector(connector);
-							if (control) {
-								var flashFirmware;
-								if (firmwarecheck) {
-									flashFirmware = setTimeout(function () {
-										sp.removeAllListeners('data');
-										connector.executeFlash = true;
-										self.finalizeScan(comName, connector, callback);
-									}, 3000);
-								}
-
-								if (control == 'master') {
-									if (extension.checkInitialData && extension.requestInitialData) {
-
-										// modify start
-										var source = sp;
-										if(sp.parser) source = sp.parser;
-										source.on('data', function (data) {
-											// modify end
-
-											// modify start
-											if(config.hardware.stream == 'string') {
-												data = data.toString();
-											}
-											// modify end
-
-											var result = extension.checkInitialData(data, config);
-											if (result === undefined) {
-												connector.send(extension.requestInitialData());
-											} else {
-												source.removeAllListeners('data'); // modify  sp --> source
-												clearTimeout(flashFirmware);
-												if (result === true) {
-
-													if (extension.setSerialPort) {
-														extension.setSerialPort(sp);
-													}
-
-													self.finalizeScan(comName, connector, callback);
-												} else if (callback) {
-													callback(new Error('Invalid hardware'));
-												}
-											}
-										});
-									}
-								} else {
-									if (duration && extension.checkInitialData && extension.requestInitialData) {
-										sp.on('data', function (data) {
-											if(config.hardware.stream == 'string') {
-												data = data.toString();
-											}
-
-											var result = extension.checkInitialData(data, config);
-											if (result !== undefined) {
-												sp.removeAllListeners('data');
-												clearTimeout(flashFirmware);
-												if (result === true) {
-													if (extension.setSerialPort) {
-														extension.setSerialPort(sp);
-													}
-													if (extension.resetProperty) {
-														connector.send(extension.resetProperty());
-													}
-													self.finalizeScan(comName, connector, callback);
-												} else if (callback) {
-													callback(new Error('Invalid hardware'));
-												}
-											}
-										});
-										var slaveTimer = self.slaveTimers[comName];
-										if (slaveTimer) {
-											clearInterval(slaveTimer);
-										}
-										slaveTimer = setInterval(function () {
-											if (self.config != config) {
-												clearInterval(slaveTimer);
-												return;
-											}
-											connector.send(extension.requestInitialData(sp));
-										}, duration);
-										self.slaveTimers[comName] = slaveTimer;
-									}
+					var connector = this.connectors[comName];
+					if (connector === undefined) {
+						connector = require('../connector/serial').create();
+						this.connectors[comName] = connector;
+						connector.open(comName, config.hardware, (error, sp) => {
+							if (error) {
+								delete this.connectors[comName];
+								if (callback) {
+									callback(error);
 								}
 							} else {
-								self.finalizeScan(comName, connector, callback);
+								this.setConnector(connector);
+								if (control) {
+									var flashFirmware;
+									if (firmwarecheck) {
+										flashFirmware = setTimeout(() => {
+											sp.removeAllListeners('data');
+											connector.executeFlash = true;
+											this.finalizeScan(comName, connector, callback);
+										}, 3000);
+									}
+
+									if (control === 'master') {
+										if (extension.checkInitialData && extension.requestInitialData) {
+
+											// modify start
+											var source = sp;
+											if(sp.parser) source = sp.parser;
+											source.on('data', (data) => {
+												// modify end
+
+												// modify start
+												if(config.hardware.stream === 'string') {
+													data = data.toString();
+												}
+												// modify end
+
+												var result = extension.checkInitialData(data, config);
+												if (result === undefined) {
+													connector.send(extension.requestInitialData());
+												} else {
+													source.removeAllListeners('data'); // modify  sp --> source
+													clearTimeout(flashFirmware);
+													if (result === true) {
+
+														if (extension.setSerialPort) {
+															extension.setSerialPort(sp);
+														}
+
+														this.finalizeScan(comName, connector, callback);
+													} else if (callback) {
+														callback(new Error('Invalid hardware'));
+													}
+												}
+											});
+										}
+									} else {
+										if (duration && extension.checkInitialData && extension.requestInitialData) {
+											sp.on('data', (data) => {
+												if(config.hardware.stream == 'string') {
+													data = data.toString();
+												}
+
+												var result = extension.checkInitialData(data, config);
+												if (result !== undefined) {
+													sp.removeAllListeners('data');
+													clearTimeout(flashFirmware);
+													if (result === true) {
+														if (extension.setSerialPort) {
+															extension.setSerialPort(sp);
+														}
+														if (extension.resetProperty) {
+															connector.send(extension.resetProperty());
+														}
+														this.finalizeScan(comName, connector, callback);
+													} else if (callback) {
+														callback(new Error('Invalid hardware'));
+													}
+												}
+											});
+											var slaveTimer = this.slaveTimers[comName];
+											if (slaveTimer) {
+												clearInterval(slaveTimer);
+											}
+											slaveTimer = setInterval(() => {
+												if (this.config !== config) {
+													clearInterval(slaveTimer);
+													return;
+												}
+												connector.send(extension.requestInitialData(sp));
+											}, duration);
+											this.slaveTimers[comName] = slaveTimer;
+										}
+									}
+								} else {
+									this.finalizeScan(comName, connector, callback);
+								}
 							}
-						}
-					});
+						});
+					}
+				}
+			});
+		});
+	};
+
+	clearTimers() {
+		if (this.timer) {
+			clearInterval(this.timer);
+			this.timer = undefined;
+		}
+		var slaveTimers = this.slaveTimers;
+		if (slaveTimers) {
+			var slaveTimer;
+			for (var key in slaveTimers) {
+				slaveTimer = slaveTimers[key];
+				if (slaveTimer) {
+					clearInterval(slaveTimer);
 				}
 			}
-		});
-	});
-};
+		}
+		this.slaveTimers = {};
+	};
 
-Scanner.prototype.clearTimers = function () {
-	if (this.timer) {
-		clearInterval(this.timer);
-		this.timer = undefined;
-	}
-	var slaveTimers = this.slaveTimers;
-	if (slaveTimers) {
-		var slaveTimer;
-		for (var key in slaveTimers) {
-			slaveTimer = slaveTimers[key];
-			if (slaveTimer) {
-				clearInterval(slaveTimer);
+	setConnector(connector) {
+		this.router.connector = connector;
+		this.router.emit('state', 'before_connect');
+	};
+
+	finalizeScan(comName, connector, callback) {
+		if (this.connectors && comName) {
+			this.connectors[comName] = undefined;
+		}
+		this.stopScan();
+
+		if (callback) {
+			callback(null, connector);
+		}
+	};
+
+	closeConnectors() {
+		var connectors = this.connectors;
+		if (connectors) {
+			var connector;
+			for (var key in connectors) {
+				connector = connectors[key];
+				if (connector) {
+					connector.close();
+				}
 			}
 		}
-	}
-	this.slaveTimers = {};
-};
-
-Scanner.prototype.setConnector = function (connector) {
-	this.router.connector = connector;
-	this.router.emit('state', 'before_connect');
-};
-
-Scanner.prototype.finalizeScan = function (comName, connector, callback) {
-	if (this.connectors && comName) {
-		this.connectors[comName] = undefined;
-	}
-	this.stopScan();
-
-	if (callback) {
-		callback(null, connector);
-	}
-};
-
-Scanner.prototype.closeConnectors = function () {
-	var connectors = this.connectors;
-	if (connectors) {
-		var connector;
-		for (var key in connectors) {
-			connector = connectors[key];
-			if (connector) {
-				connector.close();
-			}
-		}
-	}
-	this.connectors = {};
-};
+		this.connectors = {};
+	};
+}
 
 module.exports = new Scanner();
