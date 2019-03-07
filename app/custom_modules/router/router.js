c6eecc837e44308c7ec2470fd7503f07b5302071
extracold1209 <extracold1209@gmail.com>
2019-03-07 17:57:26 +0900

    router/router.js 클래스화

--- app/custom_modules/router/router.js
+++ app/custom_modules/router/router.js
@@ -1,155 +1,153 @@
 'use strict';
-const util = require('util');
 const EventEmitter = require('events').EventEmitter;
 const { ipcRenderer } = require('electron');
 
-function Router() {
-	EventEmitter.call(this);
-}
-
-util.inherits(Router, EventEmitter);
-
-Router.prototype.init = function(server) {
-	this.scanner = require('./scanner/scanner');
-	this.server = server;
-	return this;
-};
-
-Router.prototype.startScan = function(config) {
-	if(this.scanner) {
-		this.extension = require('../../modules/' + config.module);
-		this.scanner.startScan(this, this.extension, config);
+class Router extends EventEmitter {
+	constructor() {
+		super();
 	}
-};
 
-Router.prototype.stopScan = function() {
-	if(this.scanner) {
-		this.scanner.stopScan();
+	init(server) {
+		this.scanner = require('./scanner/scanner');
+		this.server = server;
+		return this;
 	}
-};
 
-Router.prototype.connect = function(connector, config) {
-	var self = this;
-	var control = config.hardware.control;
-    var duration = config.hardware.duration;
-	var advertise = config.hardware.advertise;
-	var extension = this.extension;
-	var server = this.server;
-    var type = config.hardware.type;
-    var h_type = type;
+	startScan(config) {
+		if(this.scanner) {
+			this.extension = require('../../modules/' + config.module);
+			this.scanner.startScan(this, this.extension, config);
+		}
+	};
 
-	self.connector = connector;
-    if(self.connector['executeFlash']) {
-        self.emit('state', 'flash');
-    } else if(extension && server) {
-		var handler = require('./datahandler/handler.js').create(config);
-		if(extension.init) {
-			extension.init(handler, config);
+	stopScan() {
+		if(this.scanner) {
+			this.scanner.stopScan();
 		}
-		if(extension.setSocket) {
-			extension.setSocket(server);
-		}
-		server.removeAllListeners();
-		server.on('connection', function(data, type) {
-            if(extension.socketReconnection) {
-                extension.socketReconnection();
-            }
-		});
-		server.on('data', function(data, type) {
-			handler.decode(data, type);
-			if(extension.handleRemoteData) {
-				extension.handleRemoteData(handler);
+	};
+
+	connect(connector, config) {
+		const control = config.hardware.control;
+		const duration = config.hardware.duration;
+		const advertise = config.hardware.advertise;
+		const extension = this.extension;
+		const server = this.server;
+		const type = config.hardware.type;
+		const h_type = type;
+
+		this.connector = connector;
+		if(this.connector['executeFlash']) {
+			this.emit('state', 'flash');
+		} else if(extension && server) {
+			const handler = require('./datahandler/handler.js').create(config);
+			if(extension.init) {
+				extension.init(handler, config);
 			}
-		});
-		server.on('close', function() {
-			if(extension.reset) {
-				extension.reset();
+			if(extension.setSocket) {
+				extension.setSocket(server);
 			}
-		});
-        connector.connect(extension, function(state, data) {
-			if(state) {
-				self.emit('state', state);
-                if(extension.eventController) {
-                    extension.eventController(state);
-                }
-			} else {
-				if(extension.handleLocalData) {
-					extension.handleLocalData(data);
+			server.removeAllListeners();
+			server.on('connection', function(data, type) {
+				if(extension.socketReconnection) {
+					extension.socketReconnection();
 				}
-				if(extension.requestRemoteData) {
-					extension.requestRemoteData(handler);
-					var data = handler.encode();
-					if(data) {
-						server.send(data);
+			});
+			server.on('data', function(data, type) {
+				handler.decode(data, type);
+				if(extension.handleRemoteData) {
+					extension.handleRemoteData(handler);
+				}
+			});
+			server.on('close', function() {
+				if(extension.reset) {
+					extension.reset();
+				}
+			});
+			connector.connect(extension, (state, data) => {
+				if(state) {
+					this.emit('state', state);
+					if(extension.eventController) {
+						extension.eventController(state);
+					}
+				} else {
+					if(extension.handleLocalData) {
+						extension.handleLocalData(data);
+					}
+					if(extension.requestRemoteData) {
+						extension.requestRemoteData(handler);
+						const data = handler.encode();
+						if(data) {
+							server.send(data);
+						}
+					}
+					if(control === 'master') {
+						if(extension.requestLocalData) {
+							const data = extension.requestLocalData();
+							if(data) {
+								connector.send(data);
+							}
+						}
 					}
 				}
-				if(control === 'master') {
+			});
+
+			if(duration && control !== 'master') {
+				ipcRenderer.send('startRequestLocalData', duration);
+				ipcRenderer.removeAllListeners('sendingRequestLocalData');
+				ipcRenderer.on('sendingRequestLocalData', () => {
+					console.log('requestLocalData', Date.now());
 					if(extension.requestLocalData) {
-						var data = extension.requestLocalData();
+						const data = extension.requestLocalData();
 						if(data) {
 							connector.send(data);
 						}
 					}
-				}
+					if(extension.getProperty) {
+						const data = extension.getProperty();
+						if(data) {
+							connector.send(data);
+						}
+					}
+				});
 			}
-		});
 
-        if(duration && control !== 'master') {
-        	ipcRenderer.send('startRequestLocalData', duration);
-			ipcRenderer.removeAllListeners('sendingRequestLocalData');
-			ipcRenderer.on('sendingRequestLocalData', () => {
-				console.log('requestLocalData', Date.now());
-				if(extension.requestLocalData) {
-					const data = extension.requestLocalData();
+			if(advertise) {
+				this.advertise = setInterval(function () {
+					const data = handler.encode();
 					if(data) {
-						connector.send(data);
+						server.send(data);
 					}
-				}
-				if(extension.getProperty) {
-					const data = extension.getProperty();
-					if(data) {
-						connector.send(data);
-					}
-				}
-			});
-        }
-
-        if(advertise) {
-            self.advertise = setInterval(function () {
-                var data = handler.encode();
-                if(data) {
-                    server.send(data);
-                }
-            }, advertise);
-        }
-	}
-};
-
-Router.prototype.close = function() {
-	if(this.server) {
-		this.server.disconnectHardware();
-		this.server.removeAllListeners();
-	}
-	if(this.scanner) {
-		this.scanner.stopScan();
-	}
-	if(this.connector) {
-		console.log('disconnect');
-		if(this.extension.disconnect) {
-			this.extension.disconnect(this.connector);
-		} else {
-			this.connector.close();
+				}, advertise);
+			}
 		}
-	}
-    // if(this.timer) {
-    //     clearInterval(this.timer);
-    //     this.timer = undefined;
-    // }
-	ipcRenderer.send('stopRequestLocalData');
-	if(this.advertise) {
-		clearInterval(this.advertise);
-		this.advertise = undefined;
-	}
-};
+	};
+
+	close() {
+		if(this.server) {
+			this.server.disconnectHardware();
+			this.server.removeAllListeners();
+		}
+		if(this.scanner) {
+			this.scanner.stopScan();
+		}
+		if(this.connector) {
+			console.log('disconnect');
+			if(this.extension.disconnect) {
+				this.extension.disconnect(this.connector);
+			} else {
+				this.connector.close();
+			}
+		}
+		// if(this.timer) {
+		//     clearInterval(this.timer);
+		//     this.timer = undefined;
+		// }
+		ipcRenderer.send('stopRequestLocalData');
+		if(this.advertise) {
+			clearInterval(this.advertise);
+			this.advertise = undefined;
+		}
+	};
+}
 
 module.exports = new Router();
