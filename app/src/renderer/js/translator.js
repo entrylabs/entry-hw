399d72477c6b7c19c6782ba4f834eb973387463a
extracold1209 <extracold1209@gmail.com>
2019-04-17 18:30:14 +0900

    translator.js 잘못된 로직 수정

--- app/src/renderer/js/translator.js
+++ app/src/renderer/js/translator.js
@@ -11,8 +11,7 @@ module.exports = class {
         let selectedLang = lang || 'ko';
         const browserLanguage = window.navigator.userLanguage || window.navigator.language;
         if (browserLanguage) {
-            browserLanguage.substr(0, 2);
-            selectedLang = browserLanguage;
+            selectedLang = browserLanguage.substr(0, 2);
         }
         // else { selectedLang = 'en' }
         // 기존 로직은 위와 같으나 translations.json 구조상 key 가 en 언어셋이므로 key 를 리턴하기로 함.
