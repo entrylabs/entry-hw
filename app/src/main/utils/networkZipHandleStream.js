926c8e70fe93206185917ca3127cac45c50b8bd4
extracold1209 <extracold1209@gmail.com>
2019-05-14 18:36:47 +0900

    unzipper -> tar 로 교체 (압축 포맷을 tar 로 통일)

--- app/src/main/utils/networkZipHandleStream.js
+++ app/src/main/utils/networkZipHandleStream.js
@@ -1,7 +1,7 @@
 const Stream = require('stream');
 const fs = require('fs');
 const path = require('path');
-const unzipper = require('unzipper');
+const tar = require('tar');
 
 /**
  * 네트워크를 통해 들어온 압축파일을 받아 targetPath 에 바로 압축을 푸는 스트림이다.
@@ -14,24 +14,37 @@ module.exports = class NetworkZipHandleStream extends Stream.PassThrough {
         super();
 
         const fileList = [];
+        const fileWriteStreamPromises = [];
+        const tarParse = new tar.Parse();
         // eslint-disable-next-line new-cap
-        this.pipe(unzipper.Parse())
-            .on('entry', (entry) => {
+        this.on('error', (e) => {
+            throw e;
+        });
+
+        this.pipe(tarParse);
+        tarParse.on('entry', (entry) => {
                 const type = entry.type;
                 const fileName = entry.path;
                 if (type === 'File') {
                     fileList.push(fileName);
-                    return entry.pipe(fs.createWriteStream(path.join(targetPath, fileName)));
+                    const fileWriteStream = fs.createWriteStream(path.join(targetPath, fileName));
+                    fileWriteStreamPromises.push(new Promise((resolve, reject) => {
+                        fileWriteStream.on('error', reject);
+                        fileWriteStream.on('finish', resolve);
+                    }));
+                    entry.pipe(fileWriteStream);
                 } else {
-                    return entry.autodrain();
+                    entry.autodrain();
                 }
-            })
-            .promise()
-            .then(() => {
-                this.emit('done', fileList);
-            })
-            .catch((e) => {
-                this.emit('error', e);
             });
+        tarParse.on('close', () => {
+            Promise.all(fileWriteStreamPromises)
+                .then(() => {
+                    this.emit('done', fileList);
+                })
+                .catch((e) => {
+                    this.emit('error', e);
+                });
+        });
     }
 };
