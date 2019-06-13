47551ceeb70b568650cfcbd7b81ed644cff90947
extracold1209 <extracold1209@gmail.com>
2019-06-13 18:50:20 +0900

    rendererConsole ts화

---
+++ app/src/main/utils/rendererConsole.ts
@@ -0,0 +1,35 @@
+import { BrowserWindow, webContents } from 'electron';
+
+module.exports = class {
+    static sender: webContents;
+    /**
+     * @param rendererWindow{BrowserWindow}
+     */
+    static initialize(rendererWindow: BrowserWindow) {
+        this.sender = rendererWindow.webContents;
+    }
+
+    static log(contents: any, ...args: any[]) {
+        if (!this.sender.isDestroyed()) {
+            this.sender.send('console', contents, ...args);
+        }
+    }
+
+    static info(contents: any) {
+        if (!this.sender.isDestroyed()) {
+            this.sender.send('console', `%c${contents}`, 'color: dodgerblue');
+        }
+    }
+
+    static warn(contents: any) {
+        if (!this.sender.isDestroyed()) {
+            this.sender.send('console', `%c${contents}`, 'color: orange');
+        }
+    }
+
+    static error(contents: any, error: any) {
+        if (!this.sender.isDestroyed()) {
+            this.sender.send('console', `%c${contents}`, 'color: red', error);
+        }
+    }
+};
