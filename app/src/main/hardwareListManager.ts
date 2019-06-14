d906f965649b468a9e9d023c825d77bdfb32898f
extracold1209 <extracold1209@gmail.com>
2019-06-14 11:20:23 +0900

    hardwareListManager ts화

---
+++ app/src/main/hardwareListManager.ts
@@ -0,0 +1,45 @@
+import fs from 'fs';
+import path from 'path';
+
+module.exports = class {
+    moduleBasePath: string;
+    allHardwareList: HardwareModule[];
+
+    constructor() {
+        this.moduleBasePath = path.resolve(__dirname, '..', '..', 'modules');
+        this.allHardwareList = [];
+        this.initialize();
+    }
+
+    /**
+     * 파일을 읽어와 리스트에 작성한다.
+     */
+    initialize() {
+        // noinspection JSCheckFunctionSignatures
+        try {
+            fs.readdirSync(this.moduleBasePath)
+                .filter((file) => !!file.match(/\.json$/))
+                .map((file) => {
+                    const fileContents = fs.readFileSync(path.join(this.moduleBasePath, file));
+                    return JSON.parse(fileContents.toString());
+                })
+                .filter((config) => config.platform &&
+                    config.platform.indexOf(process.platform) > -1)
+                .sort((left, right) => {
+                    const lName = left.name.ko.trim();
+                    const rName = right.name.ko.trim();
+
+                    if (lName > rName) {
+                        return 1;
+                    } else if (lName < rName) {
+                        return -1;
+                    } else {
+                        return 0;
+                    }
+                })
+                .forEach((config) => this.allHardwareList.push(config));
+        } catch (e) {
+            console.error('error occurred while reading module json files');
+        }
+    }
+};
