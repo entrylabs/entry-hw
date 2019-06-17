f81897d94ecb45b6fcc29aacc84c0973d87f8e27
extracold1209 <extracold1209@gmail.com>
2019-06-17 18:37:40 +0900

    constants 는 js 파일도 git 에 포함하도록 수정
    - tsc 가 아닌 webpack / ts-node 등의 빌드환경에서 common 으로 쓰기 위함

---
+++ app/src/common/constants.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SERVER_MODE_TYPES = {
+    single: 0,
+    multi: 1,
+    parent: 2,
+    child: 3,
+};
+exports.HARDWARE_STATEMENT = {
+    lost: 'lost',
+    disconnected: 'disconnected',
+    selectPort: 'select_port',
+    flash: 'flash',
+    beforeConnect: 'before_connect',
+    connected: 'connected',
+    showRobot: 'show_robot',
+};
+exports.AVAILABLE_TYPE = {
+    available: 'available',
+    needUpdate: 'needUpdate',
+    needDownload: 'needDownload',
+};
+//# sourceMappingURL=constants.js.map
