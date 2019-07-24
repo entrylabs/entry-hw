96527cc7e64be6949ed83bbdbfa1c907344a07c3
Ryu <ryujt658@hanmail.net>
2019-07-24 20:02:14 +0900

    블록 한 번만 실행되는 에러 수정

--- app/modules/asomebot.js
+++ app/modules/asomebot.js
@@ -71,7 +71,7 @@ class AsomeBot extends BaseModule {
 
             this.msg_id = handlerData.msg_id;
             this.sendBuffer.push(Buffer.from(handlerData.msg + "\r", 'ascii'));
-            this.sendBuffer.push(Buffer.from("'#ID " + this.msg_id + "'\r", 'ascii'));
+            this.sendBuffer.push(Buffer.from("'#ID " + handlerData.msg_id + "'\r", 'ascii'));
         }
     }
 
