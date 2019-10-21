07bbf6d4cd260e7f1df7897b37e879ef04da934e
extracold1209 <extracold1209@gmail.com>
2019-10-21 19:18:13 +0900

    [feature/react_renderer] Footer 컴포넌트화

--- app/src/renderer/react/components/Footer.tsx
+++ app/src/renderer/react/components/Footer.tsx
@@ -1,14 +1,43 @@
-import React from "react";
+import React from 'react';
+import Styled from 'styled-components';
+
+const FooterContainer = Styled.div`
+    flex: none;
+    width: 100%;
+    height: 60px;
+    background-position: 30px 15px;
+    background-repeat: no-repeat;
+    background-image: url('../images/logo.png');
+    background-color: white;
+`;
+
+const VersionLabel = Styled.div`
+    text-align: right;
+    margin: 20px 20px 20px 10px;
+    color: #595757;
+    cursor: pointer;
+    font-weight: bold;
+    float: right;
+`;
+
+const OpenSourceLabel = Styled.div`
+    text-align: right;
+    margin: 20px 10px 20px 20px;
+    color: #595757;
+    cursor: pointer;
+    font-weight: bold;
+    float: right;
+`;
 
 export default () => {
     return (
-        <div id="footer">
-            <div id="version_label" className="version_label">
+        <FooterContainer>
+            <VersionLabel id="version_label">
                 버전 정보
-            </div>
-            <div id="opensource_label" className="opensource_label">
+            </VersionLabel>
+            <OpenSourceLabel id="opensource_label">
                 오픈소스 라이선스
-            </div>
-        </div>
+            </OpenSourceLabel>
+        </FooterContainer>
     );
 }
