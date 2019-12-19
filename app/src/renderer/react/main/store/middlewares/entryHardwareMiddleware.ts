41b87760fe150bbc31edf3b5d3866a286cd20aec
extracold1209 <extracold1209@gmail.com>
2019-12-19 15:16:18 +0900

    ev3_hid 이미지 변경, 개발완료되지 않은 ble 모듈 인식 못하게 이동

--- app/src/renderer/react/main/store/middlewares/entryHardwareMiddleware.ts
+++ app/src/renderer/react/main/store/middlewares/entryHardwareMiddleware.ts
@@ -19,6 +19,7 @@ const { translator, rendererRouter } = window;
  * RendererRouter 의 ipc 통신 로직을 담당한다.
  * 리액트 엘리먼트에서는 뷰 전환에 관련한 로직만 생각하고, 여기서는 메인프로세스와의 통신만을 다룬다.
  */
+// eslint-disable-next-line max-len
 const entryHardwareMiddleware: Middleware = ({ getState }: { getState: () => IStoreState }) => (next: Dispatch<AnyAction>) => (action: AnyAction) => {
     const { type } = action; // currentAction
 
@@ -27,7 +28,11 @@ const entryHardwareMiddleware: Middleware = ({ getState }: { getState: () => ISt
             next(action);
             const { hardware } = getState();
             const { hardwareFilterKeyword, hardwareFilterCategory } = hardware;
-            const hardwareList = filterHardwareList(hardwareFilterKeyword, hardwareFilterCategory, rendererRouter.hardwareList);
+            const hardwareList = filterHardwareList(
+                hardwareFilterKeyword,
+                hardwareFilterCategory,
+                rendererRouter.hardwareList,
+            );
             changeHardwareList(next)(hardwareList);
             break;
         }
@@ -35,7 +40,11 @@ const entryHardwareMiddleware: Middleware = ({ getState }: { getState: () => ISt
             const { hardware } = getState();
             const { hardwareFilterCategory } = hardware;
             const { payload: keyword } = action;
-            const hardwareList = filterHardwareList(keyword, hardwareFilterCategory, rendererRouter.hardwareList);
+            const hardwareList = filterHardwareList(
+                keyword,
+                hardwareFilterCategory,
+                rendererRouter.hardwareList,
+            );
 
             if (!hardwareList || hardwareList.length === 0) {
                 alert(translator.translate('No results found'));
