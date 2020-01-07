8dc20a04ccae7719718b8d58d533ef3bdf0ac852
extracold1209 <extracold1209@gmail.com>
2020-01-07 14:11:11 +0900

    [1.9.3] 마이크로빗의 경우 현재 연결상태와 상관없이 펌웨어 업로드 가능하도록 수정

--- app/src/renderer/react/main/store/middlewares/entryHardwareMiddleware.ts
+++ app/src/renderer/react/main/store/middlewares/entryHardwareMiddleware.ts
@@ -11,7 +11,13 @@ import filterHardwareList from '../../functions/filterHardware';
 import { changeAlertMessage, CURRENT_PAGE_STATE_CHANGED } from '../modules/common';
 import { HardwareConnectionStatusEnum, HardwarePageStateEnum } from '../../constants/constants';
 import refreshPriorHardwareList from '../../functions/refreshPriorHardwareList';
-import { changePortList, FIRMWARE_INSTALL_REQUESTED, HARDWARE_SELECTED, PORT_SELECTED } from '../modules/connection';
+import {
+    changePortList,
+    changeVisiblePortList,
+    FIRMWARE_INSTALL_REQUESTED,
+    HARDWARE_SELECTED,
+    PORT_SELECTED,
+} from '../modules/connection';
 
 const { translator, rendererRouter } = window;
 
@@ -104,7 +110,14 @@ const entryHardwareMiddleware: Middleware = ({ getState }: { getState: () => ISt
                         });
                     })
                     .catch(() => {
-                        changeAlertMessage(next)({ message: translator.translate('Failed Firmware Upload') });
+                        changeAlertMessage(next)({
+                            message: translator.translate('Failed Firmware Upload'),
+                        });
+                    })
+                    .finally(() => {
+                        if (action.payload.type === 'copy') {
+                            changeVisiblePortList(next)(false);
+                        }
                     });
             }
             break;
