5a466137e929b7a32442ba8af1e3a95d7036f91a
extracold1209 <extracold1209@gmail.com>
2020-01-22 14:13:17 +0900

    webpack build 결과가 포함되지 않던 부분 수정 및 repo env 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ git config user.email "entrydev@nts-corp.com"
 targetBranchName="deploy/${GITHUB_REF##*/}"
 
 git checkout -b "$targetBranchName"
-git push --delete "https://${GITHUB_TOKEN}@github.com/entrylabs/entry-hw" "$targetBranchName"
-git add .
+git push --delete "https://github.com/${GITHUB_REPO}" "$targetBranchName"
+git add . --all
 git commit -m "Entry-HW for electron embed into $targetBranchName"
-git push --force --quiet "https://${GITHUB_TOKEN}@github.com/entrylabs/entry-hw" "$targetBranchName"
+git push --force --quiet "https://github.com/${GITHUB_REPO}" "$targetBranchName"
