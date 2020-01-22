45e4c11f42bf18440036a7598a27f5b3e277b5cf
extracold1209 <extracold1209@gmail.com>
2020-01-22 11:51:40 +0900

    리얼 깃 코드 시도

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-#rm .gitignore
-#
-#git config user.name "Entry Dev"
-#git config user.email "entrydev@nts-corp.com"
+rm .gitignore
+
+git config user.name "Entry Dev"
+git config user.email "entrydev@nts-corp.com"
 
 targetBranchName="deploy/${GITHUB_REF##*/}"
 
-#git checkout -b "$targetBranchName"
-#git push --delete
-
-echo ${targetBranchName}
-export
+git checkout -b "$targetBranchName"
+git push --delete "https://${GITHUB_TOKEN}@github.com/entrylabs/entry-hw" "$targetBranchName"
+git add .
+git commit -m "Entry-HW for electron embed into $targetBranchName"
+git push --force --quiet "https://${GITHUB_TOKEN}@github.com/entrylabs/entry-hw" "$targetBranchName"
