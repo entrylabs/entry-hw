71559cb02e87c70032bc121e113a78772bb74144
extracold1209 <extracold1209@gmail.com>
2020-01-22 15:41:09 +0900

    일단 build 결과가 포함된 상태로 force push (무필요 파일 삭제 안함)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,6 +9,11 @@ targetBranchName="deploy/${GITHUB_REF##*/}"
 
 git checkout -b "$targetBranchName"
 git push --delete "https://github.com/${GITHUB_REPO}" "$targetBranchName"
-git add . --all
+
+# TODO rm all unrelated files
+git add -f app/src/main/dist/**
+git add -f app/src/preload/**
+git add -f app/src/renderer/**
+
 git commit -m "Entry-HW for electron embed into $targetBranchName"
 git push --force --quiet "https://github.com/${GITHUB_REPO}" "$targetBranchName"
