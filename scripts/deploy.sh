b66da516a5176ab57b68a3f22262b47265b64c81
extracold1209 <extracold1209@gmail.com>
2020-01-22 19:08:04 +0900

    dist 결과물 제출 기준 변경
    - deploy 로 시작하는 브랜치
    - 결과물은 dist

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ rm .gitignore
 git config user.name "Entry Dev"
 git config user.email "entrydev@nts-corp.com"
 
-targetBranchName="deploy/${GITHUB_REF##*/}"
+targetBranchName="dist/${GITHUB_REF##*/}"
 
 git checkout -b "$targetBranchName"
 git push --delete "https://github.com/${GITHUB_REPO}" "$targetBranchName"
