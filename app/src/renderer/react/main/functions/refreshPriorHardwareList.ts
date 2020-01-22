3332ad376e42a2ca84dd24dd341c0557087f284a
Entry Dev <entrydev@nts-corp.com>
2020-01-22 19:26:27 +0900

    Build for electron embed

--- app/src/renderer/react/main/functions/refreshPriorHardwareList.ts
+++
@@ -1,9 +0,0 @@
-export default (newHardwareName: string) => {
-    const prevHardwareList: Array<string> = JSON.parse(localStorage.getItem('hardwareList') || '[]');
-    const foundPrevHardwareIndex = prevHardwareList.findIndex((hardwareName) => hardwareName === newHardwareName);
-    if (foundPrevHardwareIndex > -1) {
-        prevHardwareList.splice(foundPrevHardwareIndex, 1);
-    }
-    prevHardwareList.push(newHardwareName);
-    localStorage.setItem('hardwareList', JSON.stringify(prevHardwareList));
-};
