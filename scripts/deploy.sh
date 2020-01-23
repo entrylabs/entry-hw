1e69290afd8593f6bf4d648e22e1be31618ea274
extracold1209 <extracold1209@gmail.com>
2020-01-23 12:14:43 +0900

    작업중..

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,6 @@
 
 rm .gitignore
 
-git config user.name "Entry Dev"
-git config user.email "entrydev@nts-corp.com"
-
 targetBranchName="dist/${GITHUB_REF##*/}"
 
 echo "into build-base directory"
@@ -12,10 +9,9 @@ cd build-base
 
 echo "checkout $targetBranchName and delete remote repository"
 git checkout -b "$targetBranchName"
-git push --delete "https://${ACCESS_TOKEN}@github.com/entrylabs/entry-hw" "$targetBranchName"
 
 echo "forceful add files to git"
 git add -f .
 
 git commit -m "Entry-HW for electron embed into $targetBranchName"
-git push --force --quiet "https://${ACCESS_TOKEN}@github.com/entrylabs/entry-hw" "$targetBranchName"
+git push --force --quiet "https://github.com/entrylabs/entry-hw" "$targetBranchName"
