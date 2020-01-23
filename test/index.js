6c826298735b2f338b48f2aa2423882277364ff0
extracold1209 <extracold1209@gmail.com>
2020-01-23 17:39:25 +0900

    cli test / 하드웨어 모듈 선택 및 시리얼포트 선택까지 구현

---
+++ test/index.js
@@ -0,0 +1,98 @@
+const inquirer = require('inquirer');
+const _ = require('lodash');
+const path = require('path');
+const fs = require('fs').promises;
+const serialport = require('serialport');
+
+const SerialScanner = require('../app/src/main/serial/scanner');
+const SerialConnector = require('../app/src/main/serial/connector');
+
+const modulesDirPath = path.join(__dirname, '..', 'app', 'modules');
+const isDebugMode = process.argv.some((arg) => arg === '--verbose');
+
+const printError = (e, msg) => {
+    if (isDebugMode) {
+        console.error(e);
+    }
+    console.error(msg);
+};
+
+/*
+해야할일
+1. 인자로서 모듈명을 받는다
+2. 모듈명으로 json 파일을 찾는다
+3. 찾은 파일을 config 으로 등록한다
+4. 포트를 스캔한다
+5. 포트스캔 하고 scanner 에서 선택된 친구는 노란색으로 표기한다
+6.
+ */
+const getHardwareConfig = async () => {
+    const { question: answer } = await inquirer.prompt([{
+        name: 'question',
+        message: 'Select Module JSON file name',
+        default: 'arduinoExt',
+    }]);
+    try {
+        const fileBuffer = await fs.readFile(path.join(modulesDirPath, `${answer}.json`));
+        return JSON.parse(fileBuffer.toString());
+    } catch (e) {
+        printError(e, 'JSON file not found!');
+    }
+};
+
+const getHardwareModule = (config) => {
+    try {
+        if (config) {
+            return require(`../app/modules/${config.module}`);
+        }
+    } catch (e) {
+        printError(e, 'module js file not found!\nif exists, probably \'module\' property is not present');
+    }
+};
+
+const getComPort = async (config) => {
+    const scanner = new SerialScanner();
+    let selectedPort = undefined;
+
+    while (!selectedPort) {
+        const portList = await serialport.list();
+
+        // see serial/scanner.js#L104
+        const selectedPorts = [];
+        selectedPorts.push(
+            ..._.compact(
+                portList.map((port) => scanner._selectCOMPortUsingProperties(config.hardware, port)),
+            ));
+
+        const message = selectedPorts.length
+            ? `Select COM Port\n  Auto Selected Port is ${selectedPorts.join(',')}`
+            : 'Select COM Port';
+
+        const { question: answer } = await inquirer.prompt([{
+            type: 'list',
+            name: 'question', // key
+            message,
+            choices: [
+                ...portList.map((port) => port.path),
+                new inquirer.Separator(),
+                'rescan',
+            ], // value
+        }]);
+
+        if (answer !== 'rescan') {
+            selectedPort = portList.find((port) => port.path === answer);
+        }
+    }
+
+    return selectedPort;
+};
+
+// main code
+(async () => {
+    const config = await getHardwareConfig() || (process.exit(0));
+    const hwModule = getHardwareModule(config) || (process.exit(0));
+    const comPort = await getComPort(config) || (process.exit(0));
+
+    console.log(config);
+    // const selectedPort = await getComPort();
+})();
