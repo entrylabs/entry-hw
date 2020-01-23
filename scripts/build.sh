084ed69cee058189384b1993ea896e44c692a729
extracold1209 <extracold1209@gmail.com>
2020-01-23 11:49:00 +0900

    이건 되나

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
-git config --global user.name "Entry Dev"
-git config --global user.email "entrydev@nts-corp.com"
+git config --global user.name "Entry Dev";
+git config --global user.email "entrydev@nts-corp.com";
 
 echo 'Clone BuildBase..'
-git clone -b build-base "https://${GITHUB_TOKEN}@github.com/entrylabs/entry-hw" build-base
+git clone -b build-base "https://github.com/entrylabs/entry-hw" build-base;
 
 echo 'Clean BuildBase..'
-rm -rf build-base/**/*
+rm -rf build-base/**/*;
 
 echo 'Copy from projects..'
-rsync -rR app/drivers build-base
-rsync -rR app/firmwares build-base
-rsync -rR app/modules build-base
-rsync -rR app/server build-base
-rsync -rR app/src/main/mainRouter.build.js build-base
-rsync -rR app/src/preload/lang build-base
-rsync -rR app/src/preload/preload.bundle.js build-base
-rsync -rR app/src/renderer/modal build-base
-rsync -rR app/src/renderer/react/dist build-base
-rsync -rR app/src/views build-base
-rsync -rR app/OPENSOURCE.md build-base
-rsync -rR config build-base
-rsync -rR LICENSE build-base
-rsync -rR package.json build-base
+rsync -rR app/drivers build-base;
+rsync -rR app/firmwares build-base;
+rsync -rR app/modules build-base;
+rsync -rR app/server build-base;
+rsync -rR app/src/main/mainRouter.build.js build-base;
+rsync -rR app/src/preload/lang build-base;
+rsync -rR app/src/preload/preload.bundle.js build-base;
+rsync -rR app/src/renderer/modal build-base;
+rsync -rR app/src/renderer/react/dist build-base;
+rsync -rR app/src/views build-base;
+rsync -rR app/OPENSOURCE.md build-base;
+rsync -rR config build-base;
+rsync -rR LICENSE build-base;
+rsync -rR package.json build-base;
