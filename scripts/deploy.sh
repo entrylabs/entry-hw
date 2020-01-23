b2f92c143881bd119ba9a08bddf61a16bfa2c45b
extracold1209 <extracold1209@gmail.com>
2020-01-23 12:04:48 +0900

    access token 적용

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,10 +12,10 @@ cd build-base
 
 echo "checkout $targetBranchName and delete remote repository"
 git checkout -b "$targetBranchName"
-git push --delete "https://github.com/entrylabs/entry-hw" "$targetBranchName"
+git push --delete "https://${ACCESS_TOKEN}@github.com/entrylabs/entry-hw" "$targetBranchName"
 
 echo "forceful add files to git"
 git add -f .
 
 git commit -m "Entry-HW for electron embed into $targetBranchName"
-git push --force --quiet "https://github.com/entrylabs/entry-hw" "$targetBranchName"
+git push --force --quiet "https://${ACCESS_TOKEN}@github.com/entrylabs/entry-hw" "$targetBranchName"
