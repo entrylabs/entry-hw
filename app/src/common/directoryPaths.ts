993853a917421b7039733941b399e9d044aa2ba1
extracold1209 <extracold1209@gmail.com>
2020-03-31 14:36:39 +0900

    [feature] 빌드시 asar 패키징 하지 않도록 수정, 그에 따라 모든 경로 분기 통일화
    - main 내 디렉토리 경로를 한군데서 통제하는 것으로 통일
      - electron appPath 를 기준으로 모든 위치를 기준
      - 오브젝트 export 로 최초평가만 시도
    - 서버 통신로직도, 오리지널 코드도 전부 난독화 및 바이너리화 되어있어서 경로관리를 위해 변경

---
+++ app/src/common/directoryPaths.ts
@@ -0,0 +1,19 @@
+import { app } from 'electron';
+import path from 'path';
+import os from 'os';
+
+// project's app directory path
+const rootAppPath = process.env.NODE_ENV === 'production'
+    ? path.join(app.getAppPath(), 'app')
+    : path.join(app.getAppPath(), '..');
+
+export default {
+    driver: path.join(rootAppPath, 'drivers'),
+    firmware: path.join(rootAppPath, 'firmwares'),
+    modules: path.join(rootAppPath, 'modules'),
+    config: path.join(rootAppPath, '..', 'config'),
+    server: os.type().includes('Darwin')
+        ? path.join(rootAppPath, 'server', 'mac', 'server.txt')
+        : path.join(rootAppPath, 'server', 'win', 'server.exe'),
+};
+
