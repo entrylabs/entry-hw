f9d7187d2548cfa2a042dcf0cd9c6b640d115e1b
Entry Dev <entrydev@nts-corp.com>
2020-05-20 05:29:59 +0000

    Entry-HW for electron embed into dist/v1.9.6

---
+++ app/modules/byrobot_drone_8.js
@@ -0,0 +1,22 @@
+const byrobot_base = require('./byrobot_base');
+
+
+/***************************************************************************************
+ *  BYROBOT Coding Drone
+ ***************************************************************************************/
+
+class byrobot_drone_8 extends byrobot_base
+{
+    /*
+        생성자
+    */
+    constructor()
+    {
+        super();
+
+        this.targetDevice   = 0x10;
+        this.targetDeviceID = '0F0701';
+    }
+}
+
+module.exports = new byrobot_drone_8();
