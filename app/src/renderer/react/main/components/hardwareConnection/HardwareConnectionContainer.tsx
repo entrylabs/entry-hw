7be3342aae6519f9e8fce49cfac0e74699410f9a
extracold1209 <extracold1209@gmail.com>
2020-05-29 14:33:18 +0900

    [chore] 이메일, 홈페이지 클릭시 이슈 수정
    - 클릭시 해당 컨텐츠가 아닌, 그 라인 전체중 어디를 눌러도 이벤트 동작하던
      이슈 수정

--- app/src/renderer/react/main/components/hardwareConnection/HardwareConnectionContainer.tsx
+++ app/src/renderer/react/main/components/hardwareConnection/HardwareConnectionContainer.tsx
@@ -1,20 +1,21 @@
 import React, { useCallback } from 'react';
-import Styled from 'styled-components';
+import styled from 'styled-components';
 import ProgressDot from './ProgressDot';
 import withPreload from '../../hoc/withPreload';
-import { connect } from 'react-redux';
-import { IMapStateToProps } from '../../store';
+import { useSelector } from 'react-redux';
+import { IStoreState } from '../../store';
 import DriverButtonSetElement from './DriverButtonSetElement';
 import FirmwareButtonSetElement from './FirmwareButtonSetElement';
 import ComputerImage from '../../../../images/computer.png';
 
-const HardwarePanel = Styled.div`
+const HardwarePanel = styled.div`
     display: flex;
     flex-grow: 1;
     width: 100%;
 `;
 
-const ReferenceDiv = Styled.div`
+const ReferenceDiv = styled.div`
+    display: grid;
     margin-bottom: 25px;
     text-align: right;
     font-weight: bold;
@@ -23,11 +24,11 @@ const ReferenceDiv = Styled.div`
     color: #595757;
 `;
 
-const ReferenceMidDiv = Styled.div`
+const ReferenceMidDiv = styled.div`
     margin: auto;
 `;
 
-const ReferenceContentSpan = Styled.span`
+const ReferenceContentSpan = styled.span`
     width: 100%;
     height: 100%;
     text-align: left;
@@ -35,28 +36,35 @@ const ReferenceContentSpan = Styled.span`
     text-decoration: underline;
 `;
 
-const HardwarePanelElementDiv = Styled.div`
+const HardwarePanelElementDiv = styled.div`
     display: inline-block;
     height: 100%;
     text-align: center;
     vertical-align: top;
 `;
 
-const ClientElement = Styled(HardwarePanelElementDiv)`
+const ClientElement = styled(HardwarePanelElementDiv)`
     width: 210px;
 `;
 
-const HardwareElement = Styled(HardwarePanelElementDiv)`
+const HardwareElement = styled(HardwarePanelElementDiv)`
     width: 210px;
 `;
 
-const SelectedHardwareThumb = Styled.img`
+const SelectedHardwareThumb = styled.img`
     width: 135px;
     height: 135px;
 `;
 
-const HardwareConnectionContainer: React.FC<IStateProps & Preload> = (props) => {
-    const { translator, clipboard, selectedHardware, rendererRouter } = props;
+const RightBox = styled.div`
+    float: right;
+`;
+
+const HardwareConnectionContainer: React.FC<Preload> = (props) => {
+    const { translator, clipboard, rendererRouter } = props;
+    const selectedHardware = useSelector<IStoreState, IHardwareConfig | undefined>(
+        state => state.connection.selectedHardware,
+    );
     const copyString = useCallback((str: string) => {
         clipboard.writeText(str);
         alert(translator.translate('Copied to clipboard'));
@@ -74,18 +82,22 @@ const HardwareConnectionContainer: React.FC<IStateProps & Preload> = (props) => 
                 <ReferenceDiv id="reference">
                     {
                         email &&
-                        <div id="emailArea" onClick={() => {
-                            copyString(email);
-                        }}>
-                            <span>{translator.translate('E-Mail : ')}</span>
-                            <ReferenceContentSpan id="email">{email}</ReferenceContentSpan>
+                        <div id="emailArea">
+                            <RightBox onClick={() => {
+                                copyString(email);
+                            }}>
+                                <span>{translator.translate('E-Mail : ')}</span>
+                                <ReferenceContentSpan id="email">{email}</ReferenceContentSpan>
+                            </RightBox>
                         </div>
                     }
                     {
                         url &&
-                        <div id="urlArea" onClick={() => rendererRouter.openExternalUrl(url)}>
-                            <span>{translator.translate('WebSite : ')}</span>
-                            <ReferenceContentSpan id="url">{url}</ReferenceContentSpan>
+                        <div id="urlArea">
+                            <RightBox onClick={() => rendererRouter.openExternalUrl(url)}>
+                                <span>{translator.translate('WebSite : ')}</span>
+                                <ReferenceContentSpan id="url">{url}</ReferenceContentSpan>
+                            </RightBox>
                         </div>
                     }
                     {
@@ -138,13 +150,4 @@ const HardwareConnectionContainer: React.FC<IStateProps & Preload> = (props) => 
     );
 };
 
-interface IStateProps {
-    selectedHardware?: IHardwareConfig;
-}
-
-const mapStateToProps: IMapStateToProps<IStateProps> = (state) => ({
-    selectedHardware: state.connection.selectedHardware,
-});
-
-
-export default connect(mapStateToProps)(withPreload(HardwareConnectionContainer));
+export default withPreload(HardwareConnectionContainer);
