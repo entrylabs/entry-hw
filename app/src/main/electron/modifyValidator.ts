6475a589623117e0b17d7a48d8c3b83c241dca49
extracold1209 <extracold1209@gmail.com>
2020-07-09 20:31:52 +0900

    [chore] validator result 타입 수정

--- app/src/main/electron/modifyValidator.ts
+++ app/src/main/electron/modifyValidator.ts
@@ -1,6 +1,9 @@
 import fs from 'fs';
 import spawn from 'cross-spawn';
 import directoryPaths from './electronDirectoryPaths';
+import createLogger from './functions/createLogger';
+
+const logger = createLogger('Validator');
 
 /**
  * 파일 변조 여부를 검사하는 검사체
@@ -8,6 +11,17 @@ import directoryPaths from './electronDirectoryPaths';
  * 코드는 공개되어있기 때문에 누구나 사용가능하지만, 정식 릴리즈된 바이너리의 변조를 감지하기 위해 로직이 작성되었다. (사내 보안관련 권고사항)
  * 변조가 감지되면 변조가 의심된다는 알림을 표기한다. (오픈소스이기 때문에 프로그램 사용에 제재를 가하지는 않는다.)
  */
+
+export enum ResultCode {
+    VALID = 100,
+    INVALID = 200,
+    FILE_NOT_FOUND = 201,
+    NETWORK_DISCONNECTED = 300,
+    LOGIC_ERROR = 301,
+    NETWORK_INVALID = 302,
+}
+
+export type ValidationResult = { result: ResultCode; reason?: string };
 
 function isValidAsarFile(): Promise<boolean> {
     const validatorPath = directoryPaths.validator;
@@ -17,7 +31,7 @@ function isValidAsarFile(): Promise<boolean> {
     }
 
     if (!validatorPath) {
-        console.log('not asar packed environment. pass validation');
+        logger.info('not asar packed environment. pass validation');
         return Promise.resolve(true);
     }
 
@@ -33,14 +47,16 @@ function isValidAsarFile(): Promise<boolean> {
         });
 
         const timeout = setTimeout(() => {
+            logger.warn('validator spawn timeout. check validator logic');
             childProcess.kill();
             resolve(false);
         }, 3000);
 
-        childProcess.on('message', ((message) => {
-            console.log('validate result: ', message);
+        childProcess.on('message', ((message: ValidationResult) => {
             clearTimeout(timeout);
-            if (message === -2 || message === 0) {
+            const { result, reason } = message;
+            if (result === ResultCode.INVALID || result === ResultCode.FILE_NOT_FOUND) {
+                logger.warn(`validation fail reason: ${reason}`);
                 resolve(false);
             } else {
                 resolve(true);
