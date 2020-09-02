fa392909626332686e19d82c9145f192b2312d2b
extracold1209 <extracold1209@gmail.com>
2020-09-02 12:39:12 +0900

    [chore] 제발 되라

--- app/src/main/core/directoryPaths.ts
+++ app/src/main/core/directoryPaths.ts
@@ -1,12 +1,22 @@
 import path from 'path';
 
 const isProduction = process.env.NODE_ENV === 'production';
+const isForStandalone = process.env.STANDALONE === 'true';
 // project's app directory path
 // development: /Users/user/entry_projects/entry-hw/app
 // production: /Users/user/entry_projects/entry-hw/dist/mac/Entry_HW.app/Contents/Resources
-const rootAppPath = isProduction
-    ? path.join(__dirname, '..', '..', '..', '..')
-    : path.join(__dirname, '..', '..');
+const rootAppPath = (() => {
+    if (isProduction) {
+        if (isForStandalone) {
+            return path.join(__dirname, '..', '..', '..', '..');
+        } else {
+            return path.join(__dirname, '..', '..', '..', '..', '..', '..');
+        }
+    } else {
+        return path.join(__dirname, '..', '..');
+    }
+})();
+
 
 export default {
     driver: path.join(rootAppPath, 'drivers'),
