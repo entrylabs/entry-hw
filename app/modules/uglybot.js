979858bf01051ca7070737d5839c474ded26fb9b
bigstone77 <bigstone77@naver.com>
2020-09-07 23:36:36 +0900

    eslint

--- app/modules/uglybot.js
+++ app/modules/uglybot.js
@@ -24,17 +24,19 @@ Module.prototype.validateLocalData = function(data) {
 };
 
 Module.prototype.handleRemoteData = function(handler) {
-    var cmd = handler.read('CMD');
-    var ctlData = this.ctlData;
+    const cmd = handler.read('CMD');
+    const ctlData = this.ctlData;
 
-    for(var n=6; n<20; n++)
+    for (let n = 6; n < 20; n++) {
         ctlData[n] = cmd[n];
-    var sum = 0;
-    ctlData.forEach(function (value, idx) {
-        if(idx > 5)
+    }
+    let sum = 0;
+    ctlData.forEach((value, idx) => {
+        if (idx > 5) {
             sum += value;
+        }
     });
-    ctlData[5] = sum&0xFF;   
+    ctlData[5] = sum & 0xFF;   
 };
 
 Module.prototype.requestLocalData = function() {
@@ -42,14 +44,16 @@ Module.prototype.requestLocalData = function() {
 };
 
 Module.prototype.handleLocalData = function(data) {
-    var sum = 0;
-    data.forEach(function (value, idx) {
-        if(idx > 5)
+    let sum = 0;
+    data.forEach((value, idx) => {
+        if (idx > 5) {
             sum += value;
+        }
     });
-    if((data[5]==(sum&0xFF)) && (data[0] == 0x26) && (data[1] == 0xA8) && (data[2] == 0x14) && (data[3] == 0xE1)){ 
-        var sensorData = this.sensorData;
-        data.forEach(function (value, idx){
+    if ((data[5] == (sum & 0xFF)) && (data[0] == 0x26) && (data[1] == 0xA8) && 
+        (data[2] == 0x14) && (data[3] == 0xE1)) { 
+        const sensorData = this.sensorData;
+        data.forEach((value, idx) => {
             sensorData[idx] = value;
         });
     }
@@ -62,25 +66,34 @@ Module.prototype.requestRemoteData = function(handler) {
     handler.write('A3', this.sensorData[9]);
     handler.write('A4', this.sensorData[10]);  
     handler.write('A5', this.sensorData[11]);   
-    var n = this.sensorData[12];
-    if(n > 127)	n = -1*(256-n);
-    handler.write('A6', n);
-    if(this.sensorData[6] == 4)
-        handler.write('A7', this.sensorData[13]); 
-    else{
-        var n = this.sensorData[13];
-        if(n > 127)	n = -1*(256-n);
-            handler.write('A7', n);
+    let val = this.sensorData[12];
+    if (val > 127)	{
+        val = -1 * (256 - val);
+    }
+    handler.write('A6', val);
+    if (this.sensorData[6] == 4) {
+        handler.write('A7', this.sensorData[13]);
+    } else {
+        val = this.sensorData[13];
+        if (val > 127)	{
+            val = -1 * (256 - val);
+        }
+        handler.write('A7', val);
   	}	
-    var n = this.sensorData[14];
-    if(n > 127)	n = -1*(256-n);
-    handler.write('A8', n);
-    var n = this.sensorData[15];
-    if(n > 127)	n = -1*(256-n);
-    handler.write('A9', n);
+    val = this.sensorData[14];
+    if (val > 127)	{
+        val = -1 * (256 - val);
+    }
+    handler.write('A8', val);
+    
+    val = this.sensorData[15];
+    if (val > 127)	{
+        val = -1 * (256 - val);
+    }
+    handler.write('A9', val);
     handler.write('A10', this.sensorData[16]); 
     handler.write('A11', this.sensorData[17]); 
-   	handler.write("CMD", this.sensorData);
+   	handler.write('CMD', this.sensorData);
 };
 
 Module.prototype.reset = function() {
