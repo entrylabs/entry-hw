ae3c1aea12208655f8f2e2698d5c1c622cd3cf0c
gorillacell <35mwlee@naver.com>
2020-10-06 12:16:50 +0900

    GorillaCell.js error catched to var error for eslint by Remoted 2020-10-06

--- app/modules/GorillaCell.js
+++ app/modules/GorillaCell.js
@@ -23,35 +23,35 @@ Module.prototype.validateLocalData = function(data) {
 
 Module.prototype.handleRemoteData = function(handler) {
     this.readablePorts = handler.read('readablePorts');
-    var digitalValue = this.remoteDigitalValue;
-    for (var port = 0; port < 14; port++) {
+    let digitalValue = this.remoteDigitalValue;
+    for (let port = 0; port < 14; port++) {
         digitalValue[port] = handler.read(port);
     }
 };
 
 Module.prototype.requestLocalData = function() {
-    var queryString = [];
+    let queryString = [];
 
-    var readablePorts = this.readablePorts;
+    let readablePorts = this.readablePorts;
     if (readablePorts) {
-        for (var i in readablePorts) {
-            var query = (5 << 5) + (readablePorts[i] << 1);
+        for (let i in readablePorts) {
+            let query = (5 << 5) + (readablePorts[i] << 1);
             queryString.push(query);
         }
     }
-    var readablePortsValues =
+    let readablePortsValues =
         (readablePorts && Object.values(readablePorts)) || [];
-    var digitalValue = this.remoteDigitalValue;
-    for (var port = 0; port < 14; port++) {
+    let digitalValue = this.remoteDigitalValue;
+    for (let port = 0; port < 14; port++) {
         if (readablePortsValues.indexOf(port) > -1) {
             continue;
         }
-        var value = digitalValue[port];
+        let value = digitalValue[port];
         if (value === 255 || value === 0) {
-            var query = (7 << 5) + (port << 1) + (value == 255 ? 1 : 0);
+            let query = (7 << 5) + (port << 1) + (value == 255 ? 1 : 0);
             queryString.push(query);
         } else if (value > 0 && value < 255) {
-            var query = (6 << 5) + (port << 1) + (value >> 7);
+            let query = (6 << 5) + (port << 1) + (value >> 7);
             queryString.push(query);
             query = value & 127;
             queryString.push(query);
@@ -62,9 +62,9 @@ Module.prototype.requestLocalData = function() {
 
 Module.prototype.handleLocalData = function(data) {
     // data: Native Buffer
-    var pointer = 0;
-    for (var i = 0; i < 32; i++) {
-        var chunk;
+    let pointer = 0;
+    for (let i = 0; i < 32; i++) {
+        let chunk;
         if (!this.remainValue) {
             chunk = data[i];
         } else {
@@ -73,19 +73,19 @@ Module.prototype.handleLocalData = function(data) {
         }
         if (chunk >> 7) {
             if ((chunk >> 6) & 1) {
-                var nextChunk = data[i + 1];
+                let nextChunk = data[i + 1];
                 if (!nextChunk && nextChunk !== 0) {
                     this.remainValue = chunk;
                 } else {
                     this.remainValue = null;
 
-                    var port = (chunk >> 3) & 7;
+                    let port = (chunk >> 3) & 7;
                     this.analogValue[port] =
                         ((chunk & 7) << 7) + (nextChunk & 127);
                 }
                 i++;
             } else {
-                var port = (chunk >> 2) & 15;
+                let port = (chunk >> 2) & 15;
                 this.digitalValue[port] = chunk & 1;
             }
         }
@@ -93,12 +93,12 @@ Module.prototype.handleLocalData = function(data) {
 };
 
 Module.prototype.requestRemoteData = function(handler) {
-    for (var i = 0; i < this.analogValue.length; i++) {
-        var value = this.analogValue[i];
+    for (let i = 0; i < this.analogValue.length; i++) {
+        let value = this.analogValue[i];
         handler.write('a' + i, value);
     }
-    for (var i = 0; i < this.digitalValue.length; i++) {
-        var value = this.digitalValue[i];
+    for (let i = 0; i < this.digitalValue.length; i++) {
+        let value = this.digitalValue[i];
         handler.write(i, value);
     }
 };
