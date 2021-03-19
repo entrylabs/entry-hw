b2df3ab53561f4fe3eb442eb6f28bf263829c5bf
HgonKim <khg@robotis.com>
2021-03-19 14:09:37 +0900

    [충돌된 부분 수정]

--- app/modules/robotis_RB100_usb.js
+++ app/modules/robotis_RB100_usb.js
@@ -1,7 +1,7 @@
 function Module() {
     isReadDataArrived = true;
     isConnected = true;
-    var isTemp = true; // add by kjs 20170824 // is address 21 value 8?
+    isTemp = true; // add by kjs 20170824 // is address 21 value 8?
     this.addressToRead = [];
     this.varTimeout = null;
 
