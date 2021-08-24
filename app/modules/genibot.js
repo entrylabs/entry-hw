32a2366bf7d5c7cb3cdd57e433735ab5009472c4
JaewonLee_macbookpro <jaewonlee6512@naver.com>
2021-08-24 17:42:48 +0900

    Revert "Merge pull request #483 from genirobot/develop-hw"
    This reverts commit 2f53d72d9d33cbecf802768b09baa5524d30aab2, reversing
    changes made to 86cbfd4670702bbca95e054aaa8f4bfb9aaaba1f.

--- app/modules/genibot.js
+++ app/modules/genibot.js
@@ -104,8 +104,6 @@ const convertHexToSignedInt = (hex => {
     return hexString;
 });
 
-var isButtonPressed = false;
-
 class Module extends BaseModule {
     constructor() {
         super();
@@ -202,16 +200,9 @@ class Module extends BaseModule {
          handler.write('ROBOT_VERSION',this.robot.version);
  
         if (this.buttonStatus.status > 0) {
-            if(!isButtonPressed){
-                isButtonPressed = true;
-                this.buttonStatus.status = -1;
-                handler.write('BUTTON', true);
-                setTimeout(()=>{
-                    isButtonPressed = false;
-                },3000);
-            }else{
-                handler.write('BUTTON', false);
-            }
+            this.buttonStatus.status = -1;
+            handler.write('BUTTON', true);
+ 
         }else{
             handler.write('BUTTON', false);
         }
