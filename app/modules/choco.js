9ac4f528ea0381b659fbd95e29e4427b68dcc34d
tyranno <tyranno1223@gmail.com>
2024-09-07 21:30:06 +0900

    "쪼꼬 부팅후 센서 인지 처리 개선"

--- app/modules/choco.js
+++ app/modules/choco.js
@@ -307,11 +307,20 @@ class Choco extends BaseModule {
      * @param {*} handler 
      */
     requestRemoteData(handler) {
+        //sensor데이터는 상태가 바뀔때 전송한다.
+        if (
+            _.isEmpty(this.previousSensorData) ||
+            !_.isEqual(this.previousSensorData, this.sensorData)
+        ) {
+            handler.write('sensorData', this.sensorData);
+            this.previousSensorData = _.cloneDeep(this.sensorData);
+        }
+        
         if (this.executeCmd.processing === 'done') {
             this.log('requestRemoteData done', this.executeCmd.id);
 
             handler.write('msg_id', this.executeCmd.id);
-            handler.write('sensorData', this.sensorData);
+            //handler.write('sensorData', this.sensorData);
 
             this.executeCmd.id = '';
             this.executeCmd.processing = 'none';
