bb5ab12e7cef580b09b8e8b06aa8b7ebb49d8249
Entry Dev <entrydev@nts-corp.com>
2025-05-30 05:24:01 +0000

    Entry-HW for electron embed into dist/v1.9.67

---
+++ app/modules/neo_cannon.js
@@ -0,0 +1,284 @@
+function Module() {
+    this.tx_max_len = 68;
+    this.tx_data = new Array(this.tx_max_len);
+    this.PIXEL_NUM = 18;
+
+    this.sensor_data = {
+        vibe: 0,
+    };
+
+    this.worker_data = {
+        tone: 0,
+        buz_octave: 0,
+        buz_note: 0,
+        motor_state: 0,
+        led: 0,
+        shoot_state: 0,
+        d9: 0,
+        d10: 0,
+        angle_state: 0,
+        neopixel: [
+            { r: 0, g: 0, b: 0 },
+            { r: 0, g: 0, b: 0 },
+            { r: 0, g: 0, b: 0 },
+            { r: 0, g: 0, b: 0 },
+            { r: 0, g: 0, b: 0 },
+            { r: 0, g: 0, b: 0 },
+            { r: 0, g: 0, b: 0 },
+            { r: 0, g: 0, b: 0 },
+            { r: 0, g: 0, b: 0 },
+            { r: 0, g: 0, b: 0 },
+            { r: 0, g: 0, b: 0 },
+            { r: 0, g: 0, b: 0 },
+            { r: 0, g: 0, b: 0 },
+            { r: 0, g: 0, b: 0 },
+            { r: 0, g: 0, b: 0 },
+            { r: 0, g: 0, b: 0 },
+            { r: 0, g: 0, b: 0 },
+            { r: 0, g: 0, b: 0 },
+        ],
+    };
+
+    this.sensorValueSize = {
+        FLOAT: 2,
+        SHORT: 3,
+    };
+
+    this.sensorTypes = {
+        ALIVE: 0,
+        VIBE: 1,
+        TIMER: 2,
+    };
+}
+
+const NEOCANNON = {
+    TONE: 'tone',
+    MOTOR_STATE: 'motorState',
+    LED: 'led',
+    SHOOT_STATE: 'shootState',
+    D9: 'd9',
+    D10: 'd10',
+    ANGLE_STATE: 'angleState',
+    NEOPIXEL: 'neopixel',
+};
+
+Module.prototype.init = function (handler, config) {};
+
+Module.prototype.setSerialPort = function (sp) {
+    this.sp = sp;
+};
+
+Module.prototype.requestInitialData = function () {
+    const txData = this.tx_data;
+    const dataLen = this.tx_max_len;
+    txData[0] = 0xff;
+    txData[1] = 0x44;
+    txData[2] = 0x01;
+    txData[3] = 0x03;
+    for (let i = 4; i < dataLen - 2; i++) {
+        txData[i] = 0;
+    }
+    txData[dataLen - 2] = 0x4;
+    txData[dataLen - 1] = 0xa;
+    return txData;
+};
+
+Module.prototype.checkInitialData = function (data, config) {
+    return true;
+};
+
+Module.prototype.afterConnect = function (that, cb) {
+    that.connected = true;
+    if (cb) {
+        cb('connected');
+    }
+};
+
+Module.prototype.validateLocalData = function (data) {
+    return true;
+};
+
+/* 엔트리HW -> 엔트리JS */
+Module.prototype.requestRemoteData = function (handler) {
+    const sensorData = this.sensor_data;
+    for (const key in sensorData) {
+        handler.write(key, sensorData[key]);
+    }
+};
+
+/** 엔트리JS -> 엔트리HW */
+Module.prototype.handleRemoteData = function (handler) {
+    const workerData = this.worker_data;
+    let newValue;
+
+    if (handler.e(NEOCANNON.TONE)) {
+        newValue = handler.read(NEOCANNON.TONE);
+        if (newValue.data) {
+            const value = newValue.data.value;
+            workerData.buz_octave = value >> 8;
+            workerData.buz_note = value & 255;
+        } else if (newValue === 0) {
+            workerData.buz_octave = 0;
+            workerData.buz_note = 0;
+        } else {
+            workerData.buz_octave = 0;
+            workerData.buz_note = 0;
+        }
+    }
+
+    if (handler.e(NEOCANNON.MOTOR_STATE)) {
+        newValue = handler.read(NEOCANNON.MOTOR_STATE);
+
+        if (workerData.motor_state != newValue) {
+            workerData.motor_state = newValue;
+        }
+    }
+
+    if (handler.e(NEOCANNON.LED)) {
+        newValue = handler.read(NEOCANNON.LED);
+        workerData.led = newValue;
+    }
+
+    if (handler.e(NEOCANNON.SHOOT_STATE)) {
+        newValue = handler.read(NEOCANNON.SHOOT_STATE);
+        workerData.shoot_state = newValue;
+    }
+
+    if (handler.e(NEOCANNON.D9)) {
+        newValue = handler.read(NEOCANNON.D9);
+        workerData.d9 = newValue;
+    }
+
+    if (handler.e(NEOCANNON.D10)) {
+        newValue = handler.read(NEOCANNON.D10);
+        workerData.d10 = newValue;
+    }
+
+    if (handler.e(NEOCANNON.ANGLE_STATE)) {
+        newValue = handler.read(NEOCANNON.ANGLE_STATE);
+        workerData.angle_state = newValue;
+    }
+
+    if (handler.e(NEOCANNON.NEOPIXEL)) {
+        newValue = handler.read(NEOCANNON.NEOPIXEL);
+        if (newValue.data) {
+            const red = newValue.data.red;
+            const green = newValue.data.green;
+            const blue = newValue.data.blue;
+
+            if (newValue.data.num !== undefined) {
+                const num = newValue.data.num;
+                workerData.neopixel[num].r = red;
+                workerData.neopixel[num].g = green;
+                workerData.neopixel[num].b = blue;
+            } else {
+                for (let i = 0; i < this.PIXEL_NUM; i++) {
+                    workerData.neopixel[i].r = red;
+                    workerData.neopixel[i].g = green;
+                    workerData.neopixel[i].b = blue;
+                }
+            }
+        } else {
+            for (let i = 0; i < this.PIXEL_NUM; i++) {
+                workerData.neopixel[i].r = 0;
+                workerData.neopixel[i].g = 0;
+                workerData.neopixel[i].b = 0;
+            }
+        }
+    }
+
+    this.worker_data = workerData;
+};
+
+/* 엔트리HW -> 교구 */
+Module.prototype.requestLocalData = function () {
+    const workerData = this.worker_data;
+    const txData = this.tx_data;
+    let checkSum = 0;
+    const dataLen = txData.length;
+
+    txData[0] = 0xff;
+    txData[1] = 0x44;
+    txData[2] = 0x01;
+    txData[3] = 0x03;
+    txData[4] = workerData.buz_octave;
+    txData[5] = workerData.buz_note;
+    txData[6] = workerData.motor_state;
+    txData[7] = workerData.led;
+    txData[8] = workerData.shoot_state;
+    txData[9] = workerData.d9;
+    txData[10] = workerData.d10;
+    txData[11] = workerData.angle_state;
+
+    for (let i = 0; i < this.PIXEL_NUM; i++) {
+        txData[i * 3 + 12] = workerData.neopixel[i].r;
+        txData[i * 3 + 13] = workerData.neopixel[i].g;
+        txData[i * 3 + 14] = workerData.neopixel[i].b;
+    }
+
+    txData[dataLen - 1] = 0xa;
+
+    for (let i = 2; i < dataLen - 2; i++) {
+        checkSum += txData[i];
+    }
+    txData[dataLen - 2] = checkSum & 255;
+
+    this.tx_data = txData;
+
+    return txData;
+};
+
+/* 교구 -> 엔트리HW */
+Module.prototype.handleLocalData = function (data) {
+    const datas = this.getDataByBuffer(data);
+    const sensorData = this.sensor_data;
+
+    datas.forEach((data) => {
+        if (data.length <= 4 || data[0] !== 255 || data[1] !== 7) {
+            return;
+        }
+        const readData = data.subarray(2, data.length);
+        let value;
+        let checkSum;
+
+        const idx = readData[0];
+        if (idx == 1) {
+            if (readData.length != 4) {
+                return;
+            }
+            const vibeState = readData[1];
+            const life = readData[2];
+            checkSum = (idx + vibeState + life) & 0xff;
+            if (checkSum != readData[3]) return;
+
+            sensorData.vibe = vibeState;
+        }
+    });
+    this.sensor_data = sensorData;
+};
+
+Module.prototype.getDataByBuffer = function (buffer) {
+    const datas = [];
+    let lastIndex = 0;
+    buffer.forEach((value, idx) => {
+        if (value == 13 && buffer[idx + 1] == 10) {
+            datas.push(buffer.subarray(lastIndex, idx));
+            lastIndex = idx + 2;
+        }
+    });
+    return datas;
+};
+
+Module.prototype.disconnect = function (connect) {
+    connect.close();
+    if (this.sp) {
+        delete this.sp;
+    }
+};
+
+Module.prototype.reset = function () {
+    this.lastTime = 0;
+    this.lastSendTime = 0;
+};
+
+module.exports = new Module();
---
+++ app/modules/smartCodingHouse.js
@@ -0,0 +1,155 @@
+function Module() {
+    this.digitalValue = new Array(14);
+    this.analogValue = new Array(6);
+
+    this.remoteDigitalValue = new Array(14);
+    this.readablePorts = null;
+    this.remainValue = null;
+
+    // 내부 타이머 초기화 (있다면)
+    this.processInterval = null;
+}
+
+Module.prototype.init = function (handler, config) {
+    // 내부 상태 초기화
+    this.requestInitialData();
+
+    // 시리얼 통신 버퍼 flush (flush 함수가 있는 경우)
+    if (handler && typeof handler.flush === 'function') {
+        handler.flush();
+        console.log('Serial buffer flushed in init.');
+    }
+};
+
+Module.prototype.requestInitialData = function () {
+    for (var i = 0; i < 14; i++) {
+        this.digitalValue[i] = 0;
+        this.remoteDigitalValue[i] = 0;
+    }
+    for (var i = 0; i < 6; i++) {
+        this.analogValue[i] = 0;
+    }
+    this.readablePorts = null;
+    this.remainValue = null;
+    return null;
+};
+
+Module.prototype.checkInitialData = function (data, config) {
+    return true;
+};
+
+Module.prototype.validateLocalData = function (data) {
+    return true;
+};
+
+Module.prototype.handleRemoteData = function (handler) {
+    this.readablePorts = handler.read('readablePorts');
+    var digitalValue = this.remoteDigitalValue;
+    for (var port = 0; port < 14; port++) {
+        digitalValue[port] = handler.read(port);
+    }
+};
+
+Module.prototype.requestLocalData = function () {
+    var queryString = [];
+    var digitalValue = this.remoteDigitalValue;
+    for (var port = 0; port < 14; port++) {
+        var value = digitalValue[port];
+        if (value === 255 || value === 0) {
+            var query = (7 << 5) + (port << 1) + (value === 255 ? 1 : 0);
+            queryString.push(query);
+        } else if (value > 0 && value < 255) {
+            var query = (6 << 5) + (port << 1) + (value >> 7);
+            queryString.push(query);
+            query = value & 127;
+            queryString.push(query);
+        }
+    }
+
+    // console.log('[DEBUG] DC Motor value (pin):', digitalValue);
+
+    return queryString;
+};
+
+Module.prototype.handleLocalData = function (data) {
+    // data: Native Buffer
+    for (var i = 0; i < 32; i++) {
+        var chunk;
+        if (!this.remainValue) {
+            chunk = data[i];
+        } else {
+            chunk = this.remainValue;
+            i--;
+        }
+
+        if (chunk >> 7) {
+            if ((chunk >> 6) & 1) {
+                var nextChunk = data[i + 1];
+                if (nextChunk === undefined) {
+                    this.remainValue = chunk;
+                } else {
+                    this.remainValue = null;
+                    var port = (chunk >> 3) & 7;
+                    this.analogValue[port] = ((chunk & 7) << 7) + (nextChunk & 127);
+                }
+                i++;
+            } else {
+                var port = (chunk >> 2) & 15;
+                this.digitalValue[port] = chunk & 1;
+            }
+        }
+    }
+
+    // Entry.hw 및 Entry.hw.portData 초기화
+    if (typeof Entry === 'undefined') {
+        Entry = {};
+    }
+    if (!Entry.hw) {
+        Entry.hw = {};
+    }
+    if (!Entry.hw.portData) {
+        Entry.hw.portData = {};
+    }
+
+    // 아날로그, 디지털 값 저장
+    Entry.hw.portData.analog = this.analogValue;
+    Entry.hw.portData.digital = this.digitalValue;
+    // 각 아날로그 채널 a0 ~ a5 매핑
+    for (var i = 0; i < 6; i++) {
+        Entry.hw.portData['a' + i] = this.analogValue[i];
+    }
+    // 초음파 센서: 아날로그 채널 1
+    Entry.hw.portData.ultrasonic = this.analogValue[1];
+    // 온습도 센서: 아날로그 채널 2 (온도와 습도 동일값; 필요시 별도 처리)
+    Entry.hw.portData.temperature = this.analogValue[2];
+    Entry.hw.portData.humidity = this.analogValue[2];
+
+    //--test console--
+    console.log('analogValue:', this.analogValue);
+    //console.log('digitalValue:', this.digitalValue);
+
+    // DC 모터(3번 핀)의 값을 로그로 출력 (디지털 해석 결과)
+    //console.log('[DEBUG] DC Motor digital value received (pin 3):', Entry.hw.portData.digital[3]);
+};
+
+Module.prototype.requestRemoteData = function (handler) {
+    for (var i = 0; i < this.analogValue.length; i++) {
+        var value = this.analogValue[i];
+        handler.write('a' + i, value);
+    }
+    for (var i = 0; i < this.digitalValue.length; i++) {
+        var value = this.digitalValue[i];
+        handler.write(i, value);
+    }
+};
+
+Module.prototype.reset = function () {
+    if (this.processInterval) {
+        clearInterval(this.processInterval);
+        this.processInterval = null;
+    }
+    this.requestInitialData();
+};
+
+module.exports = new Module();
+ 
